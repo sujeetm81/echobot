1ad5db10f9c4e554e7214d75fe7734565445dff2
sujeetm81 <sujeetmehta@hotmail.com>
2016-04-10 08:23:33 -0700

    test

--- index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log('Got request for ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Sujeet Mehta and Azure Web Apps!</h1>');
-}).listen(process.env.PORT); 
