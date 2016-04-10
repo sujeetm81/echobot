17b9e39a0db0cedd066d16baadfa54d2bd9f12be
sujeetm81 <sujeetmehta@hotmail.com>
2016-04-09 20:04:16 -0700

    commit

--- server.js
+++ server.js
@@ -1,26 +1,7 @@
 const restify = require('restify');
-const skype = require('skype-sdk');
 
-const botService = new skype.BotService({
-    messaging: {
-        botId: '28:<bot’s id="">',
-        serverUrl : "https://apis.skype.com",
-        requestTimeout : 15000,
-        appId: '74717835-d950-4822-bfb3-85c7573f16e9',
-        appSecret: '1SkRqV6PZHATbyfvbviM0qe'
-    }
-});
-
-botService.on('contactAdded', (bot, data) => {
-    bot.reply('Hello ${data.fromDisplayName}!', true);
-});
-
-botService.on('personalMessage', (bot, data) => {
-    bot.reply('Hey ${data.from}. Thank you for your message: "${data.content}".', true);
-});
 
 const server = restify.createServer();
-server.post('/v1/chat', skype.messagingHandler(botService));
 const port = process.env.PORT || 8080;
 server.listen(port);
 console.log('Listening for incoming requests on port ' + port);
