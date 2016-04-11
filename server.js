e29b9506c88d1fd66b67a276498db9bce3286c9b
sujeetm81 <sujeetmehta@hotmail.com>
2016-04-10 20:06:06 -0700

    port

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ botService.on('personalMessage', (bot, data) => {
 
 const server = restify.createServer();
 server.post('/v1/chat', skype.messagingHandler(botService));
-const port = 8001;
+const port = 80001;
 server.listen(port);
 console.log('Listening for incoming requests on port ' + port);
